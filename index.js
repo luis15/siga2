af83c475ee44426db2962958c2f1cc80a7977632
Gustavo Gutierrez <54781049+Gustavo2022003@users.noreply.github.com>
2023-08-24 14:08:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,31 +7,36 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
-});
-
+// Este é o endpoint raiz da API e deve retornar todos os alunos
 app.get('/alunos', async (req, res) => {
   const Aluno = new alunoController(req, res);
   await Aluno.getAll();
 });
 
+// Este é o endpoint que deve retornar apenas um aluno, de acordo com o id passado na URL
 app.get('/alunos/:id', async (req, res) => {
   const Aluno = new alunoController(req, res);
   await Aluno.getAluno(req.params.id);
 });
 
+// Este é o endpoint que deve criar um novo aluno
 app.post('/postAluno', async (req, res) => {
   const Aluno = new alunoController(req, res);
   await Aluno.createAluno();
 });
 
+// Este é o endpoint que deve deletar um aluno, de acordo com o id passado na URL
 app.delete('/deleteAluno/:id', async (req, res) => {
   const Aluno = new alunoController(req, res);
   await Aluno.deleteAluno(req.params.id);
 });
 
+// Este é o endpoint que deve atualizar um aluno, de acordo com o id passado na URL
 app.patch('/updateAluno/:id', async (req, res) => {
   const Aluno = new alunoController(req, res);
   await Aluno.updateAluno(req.params.id);
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
