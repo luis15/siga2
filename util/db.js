cb1302e7ad3a51f52a9117fe04670cd002158d0f
Gustavo Gutierrez <54781049+Gustavo2022003@users.noreply.github.com>
2023-08-25 23:28:07 -0300

    OK

--- util/db.js
+++ util/db.js
@@ -1,14 +1,18 @@
 const mysql = require('mysql');
+const dotenv = require('dotenv'); // Importe a biblioteca dotenv
+dotenv.config(); // Carregue as variáveis de ambiente do arquivo .env
+
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'siga2'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
-  
+
 db.connect(err => {
     if (err) throw err;
     console.log('Conexão com o banco de dados estabelecida.');
 });
-  
+
 module.exports = db;
+
