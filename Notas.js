ffbf75b28e473d4c777766ef0265633f1d202642
Heloísa M Coletti <mangabahelo@gmail.com>
2023-08-29 22:18:42 -0300

    Update Notas.js

--- Notas.js
+++ Notas.js
@@ -1,59 +1,69 @@
 const db = require('../util/db');
 
-class Nota {
-    constructor(codigoMatricula, descritivo, valor) {
-        this.codigoMatricula = codigoMatricula;
-        this.descritivo = descritivo;
-        this.valor = valor;
-        this.codigo = this.gerarCodigoUnico();
+class NotaModel {
+    async getNotas() {
+        return new Promise((resolve, reject) => {
+            db.query("SELECT * FROM notas;", (err, result) => {
+                if (err) {
+                    throw err;
+                }
+                resolve(result);
+            });
+        });
     }
 
-    gerarCodigoUnico() {
-        return "N" + Math.floor(Math.random() * 100000);
+    async getNota(id) {
+        return new Promise((resolve, reject) => {
+            db.query(`SELECT * FROM notas WHERE id = ${id};`, (err, result) => {
+                if (err) {
+                    throw err;
+                }
+                resolve(result);
+            });
+        });
+    }
+
+    async createNota(codigoMatricula, descritivo, valor) {
+        return new Promise((resolve, reject) => {
+            db.query(
+                `INSERT INTO notas (codigoMatricula, descritivo, valor) VALUES (${codigoMatricula}, '${descritivo}', ${valor});`,
+                (err, result) => {
+                    if (err) {
+                        throw err;
+                    }
+                    resolve(result);
+                }
+            );
+        });
+    }
+
+    async updateNota(id, notaUpdate) {
+        const query = `UPDATE notas SET ${Object.keys(notaUpdate)
+            .map(key => `${key} = ?`)
+            .join(', ')} WHERE id = ?`;
+
+        const updateValues = [...Object.values(notaUpdate), id];
+
+        return new Promise((resolve, reject) => {
+            db.query(query, updateValues, (error, results) => {
+                if (error) {
+                    reject(error);
+                }
+                resolve(results);
+            });
+        });
+    }
+
+    async deleteNota(id) {
+        return new Promise((resolve, reject) => {
+            db.query(`DELETE FROM notas WHERE id = ${id};`, (err, result) => {
+                if (err) {
+                    throw err;
+                }
+                resolve(result);
+            });
+        });
     }
 }
 
-class GerenciadorNotas {
-    constructor() {
-        this.notas = [];
-    }
-
-    cadastrarNota(codigoMatricula, descritivo, valor) {
-        // Verificar matrícula e valor, criar nova nota e adicioná-la à lista
-        const novaNota = new Nota(codigoMatricula, descritivo, valor);
-        this.notas.push(novaNota);
-        return novaNota.codigo;
-    }
-
-    consultarListaNotas() {
-        return this.notas;
-    }
-
-    consultarNotaEspecifica(codigoNota) {
-        return this.notas.find(nota => nota.codigo === codigoNota);
-    }
-
-    excluirNota(codigoNota) {
-        // Excluir a nota
-        const index = this.notas.findIndex(nota => nota.codigo === codigoNota);
-        if (index !== -1) {
-            this.notas.splice(index, 1);
-            return true;
-        }
-        return false;
-    }
-}
-
-const gerenciadorNotas = new GerenciadorNotas();
-
-const codigoNotaCadastrada = gerenciadorNotas.cadastrarNota(codigoMatricula, "Prova 1", 8.5);
-console.log("Código da nota cadastrada:", codigoNotaCadastrada);
-
-const listaNotas = gerenciadorNotas.consultarListaNotas();
-console.log("Lista de notas:", listaNotas);
-
-const notaEspecifica = gerenciadorNotas.consultarNotaEspecifica(codigoNotaCadastrada);
-console.log("Nota específica:", notaEspecifica);
-
-const notaExcluida = gerenciadorNotas.excluirNota(codigoNotaCadastrada);
-console.log("Nota excluída:", notaExcluida);
+module.exports = NotaModel;
